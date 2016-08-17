16628342b1bf49c5fc78f27d5ad71537eef3e14d
aehmt <calisahmet@gmail.com>
2016-08-17 12:49:43 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,28 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
+  def show
+    @student = Student.find(params[:id])
+  end
 end
+
+
+
+# Learn Solution
+# class StudentsController < ApplicationController
+#   before_action :set_student, only: :show
+  
+#   def index
+#     @students = Student.all
+#   end
+
+#   def show
+#   end
+
+#   private
+
+#     def set_student
+#       @student = Student.find(params[:id])
+#     end
+# end
