6797a7c18b8f36b29ed911293bf63115db18e3f2
ozPop <ozziepopovas@gmail.com>
2016-08-27 17:21:38 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Daenerys", last_name: "Targaryen")
+Student.create(first_name: "Bob", last_name: "Ross")
+Student.create(first_name: "Steven", last_name: "Seagal")
